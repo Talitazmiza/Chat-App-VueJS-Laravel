ea7f587a34c0bb48a656a0fda08c3f4b8779c29f
AdityaIrfan <adityairfan2399@gmail.com>
2020-11-13 22:42:35 +0700

    project upload

---
+++ resources/views/layouts/vue.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Laravel + Vue Chat App</title>
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+	<div id="app"></div>
+	<script src="{{ asset('js/app/js') }}"></script>
+	<h1>
+		Diana, Lita, Edit Adit bersatu
+	</h1>
+</body>
+</html>
