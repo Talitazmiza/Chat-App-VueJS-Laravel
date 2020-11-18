3c61cfe2b1a951b18ca6c179a04b088b3cac441c
dianasaurus-debug <54146607+dianasaurus-debug@users.noreply.github.com>
2020-11-18 11:26:31 +0700

    menyesuaikan routes dengan yang di video

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', function () {
+Route::get('/coba', function () {
     return view('layouts.vue');
-})->where('any', '.*');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
