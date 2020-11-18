849d526e856d47e945ba81a820db54b8a8c7f296
dianasaurus-debug <54146607+dianasaurus-debug@users.noreply.github.com>
2020-11-18 10:54:35 +0700

    add messages model

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<div id="app"></div>
-	<script src="{{ asset('js/app/js') }}"></script>
+	<script src="{{ asset('js/app.js') }}"></script>
 	<h1>
 		Diana, Lita, Edit Adit bersatu
 	</h1>
