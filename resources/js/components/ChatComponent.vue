b1e79fe71200b9adac08e6be8e20f5054b90180d
Talita <talitazmiza@gmail.com>
2020-11-19 09:29:39 +0700

    message component, chat component, vue sekrol

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-3">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Private Chat App
+                    </div>
+                    <ul class="list-group">
+                            <li class="list-group-item">Lili Marceuli</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <message-component></message-component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MessageComponent from "./MessageComponent";
+    export default {
+        components:{MessageComponent},
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/MessageComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="card card-default chat-box">
+        <div class="card-header">
+            Chats
+        </div>
+        <div class="card-body" v-chat-scroll>
+            <p class="card-text" v-for="chat in chats" :key="chat.message">
+                {{chat.message}}
+            </p>
+        </div>
+        <form class="card-footer" @submit.prevent="send">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Write an message">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                chats: []
+            }
+        },
+        methods: {
+            send() {
+                console.log("yeea");
+            }
+        },
+        created(){
+            this.chats.push(
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'werrrrrrrrr r u'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'oyyyyyy'},
+                {message: 'bottom'}
+            )
+        }
+    }
+</script>
+
+<style>
+    .chat-box {
+        height: 400px;
+    }
+
+    .card-body {
+        overflow-y: scroll
+    }
+</style>
+
