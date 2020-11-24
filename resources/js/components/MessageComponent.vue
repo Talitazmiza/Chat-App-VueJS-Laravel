83206033f1051a3b32250537ff760c4e769c6eef
dianasaurus-debug <54146607+dianasaurus-debug@users.noreply.github.com>
2020-11-24 15:41:03 +0700

    add block, unblock, clear, and close

--- resources/js/components/MessageComponent.vue
+++ resources/js/components/MessageComponent.vue
@@ -1,8 +1,25 @@
 <template>
     <div class="card card-default chat-box">
-        <div class="card-header">
-            Chats
+      <div class="card-header">
+        <b :class="{'text-danger': session_blocked}">
+          {{ friend.name }}
+          <span v-if="session_blocked">(Blocked)</span>
+        </b>
+
+        <a href="" @click.prevent="close" >
+          <i class="fa fa-times float-right" style="color : red"></i>
+        </a>
+        <div class="dropdown float-right mr-4">
+          <a href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-ellipsis-v"></i>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="" @click.prevent="unblock" v-if="session_blocked">Unblock</a>
+            <a class="dropdown-item" href="" @click.prevent="block" v-else>Block</a>
+            <a class="dropdown-item" href=""  @click.prevent="clear">Clear Chat</a>
+          </div>
         </div>
+      </div>
         <div class="card-body" v-chat-scroll>
             <p class="card-text" v-for="chat in chats" :key="chat.message">
                 {{chat.message}}
@@ -10,7 +27,7 @@
         </div>
         <form class="card-footer" @submit.prevent="send">
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Write an message">
+                <input type="text" :disabled="session_blocked" class="form-control" placeholder="Write an message">
             </div>
         </form>
     </div>
@@ -18,30 +35,34 @@
 
 <script>
     export default {
+        props : ['friend'],
         data() {
             return {
-                chats: []
+                chats: [],
+                session_blocked : false,
             }
         },
         methods: {
             send() {
                 console.log("yeea");
+            },
+            close(){
+              this.$emit('close')
+            },
+            clear(){
+              this.chats = [];
+            },
+            block(){
+              this.session_blocked = true
+            },
+            unblock(){
+              this.session_blocked = false
             }
+
         },
         created(){
             this.chats.push(
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
                 {message: 'werrrrrrrrr r u'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
-                {message: 'oyyyyyy'},
                 {message: 'bottom'}
             )
         }
