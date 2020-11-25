7a1f42a83422e8605846c23568aed0f0529f8bde
AdityaIrfan <adityairfan2399@gmail.com>
2020-11-26 05:15:01 +0700

    add SessionEvent

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\SessionEvent;
 use App\Http\Resources\SessionResource;
 use App\Models\Session;
 use Illuminate\Http\Request;
@@ -11,6 +12,7 @@ class SessionController extends Controller
     public function create(Request $request){
         $session = Session::create(['user1_id'=> auth()->id(), 'user2_id' => $request->friend_id]);
         $modified_session = new SessionResource($session);
+        broadcast(new SessionEvent($modified_session, auth()->id()));
         return $modified_session;
     }
 }
