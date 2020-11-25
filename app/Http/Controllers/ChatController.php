2ef46981934c930dee4f3de68847b2afe866dcbe
AdityaIrfan <adityairfan2399@gmail.com>
2020-11-26 05:43:54 +0700

    add part of Message Model and Chat Controller for Send Message backend

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Session;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function send(Session $session, Request $request) 
+    {
+        $message = $session->message()->create(['content' => $request->content]);
+        
+        $message->createForSend($session->id);
+
+        $message->createForRecieve($session->id, $request->to_user);
+
+        return response($message, 200);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\ChatController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/coba', function () {
     return view('layouts.vue');
@@ -13,6 +15,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');   
 Route::get('/getFriends', [App\Http\Controllers\HomeController::class, 'getFriends']);
-Route::post('/session/create', 'App\Http\Controllers\SessionController@create');
+Route::post('/session/create', [SessionController::class, 'create']);
+Route::post('/send/{session}', [ChatController::class, 'send']);
