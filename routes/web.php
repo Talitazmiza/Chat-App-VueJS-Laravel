d0ae70d594b7672f8bda13b4602af1a48927a37d
dianasaurus-debug <54146607+dianasaurus-debug@users.noreply.github.com>
2020-11-26 15:47:39 +0700

    refactor the routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 
 Route::get('/coba', function () {
     return view('layouts.vue');
@@ -15,7 +16,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');   
-Route::get('/getFriends', [App\Http\Controllers\HomeController::class, 'getFriends']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/getFriends', [HomeController::class, 'getFriends']);
 Route::post('/session/create', [SessionController::class, 'create']);
 Route::post('/send/{session}', [ChatController::class, 'send']);
