72bea135ed4ee3326157c355524979310c34970b
dianasaurus-debug <54146607+dianasaurus-debug@users.noreply.github.com>
2020-12-08 10:31:05 +0700

    in case males nambahin user tak kasih user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,46 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        User::factory()->count(3)->create(
+//        User::factory()->count(3)->create(
         	// [
         	// 	'name' => 'aditya',
         	// 	'email' => 'aditya@aditya.com',
         	// ]
-        ); // 3 merupakan banyaknya data yang akan di masukkan
+        //); // 3 merupakan banyaknya data yang akan di masukkan
+        User::factory()->create(
+            [
+            'name'  => 'leon',
+            'email' => 'leon@gmail.com',
+            'password'  => bcrypt('password')
+            ],
+        );
+        User::factory()->create(
+            [
+                'name'  => 'ada wong',
+                'email' => 'ada@gmail.com',
+                'password'  => bcrypt('password')
+            ]
+        );
+        User::factory()->create(
+            [
+                'name'  => 'helena harper',
+                'email' => 'helena@gmail.com',
+                'password'  => bcrypt('password')
+            ]
+        );
+        User::factory()->create(
+            [
+                'name'  => 'jill valentine',
+                'email' => 'jill@gmail.com',
+                'password'  => bcrypt('password')
+            ]
+        );
+        User::factory()->create(
+            [
+                'name'  => 'chris redfield',
+                'email' => 'chris@gmail.com',
+                'password'  => bcrypt('password')
+            ]
+        );
     }
 }
